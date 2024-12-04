8a73bcaf7742d8d4b9c9522cf36d506830db817f
Dev goyal <158808178+fafawds67685da@users.noreply.github.com>
2024-12-04 17:03:25 +0530

    Create 6. + overloading ( Friend func).cpp

---
+++ cpp practical prep/6. + overloading ( Friend func).cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class abc
+{
+public:
+    int a;
+    abc(){}
+
+    abc(int x)
+    {
+        a=x;
+    }
+
+    friend abc operator +(abc&, abc&);
+
+
+    void display()
+    {
+        cout<<a<<" ";
+    }
+};
+
+    abc operator +(abc& ob1, abc& ob2)
+    {
+        return ob1.a+ ob2.a;
+    }
+
+
+int main()
+{
+    abc ob(10),ob3(10),ob4;
+    ob4 = ob+ob3;
+    ob4.display();
+    return 0;
+}
