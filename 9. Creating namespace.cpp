d4232c1700c79f92eab76cf02b87220442ce9902
Dev goyal <158808178+fafawds67685da@users.noreply.github.com>
2024-12-27 11:16:43 +0530

    Create 9. Creating namespace.cpp

---
+++ 9. Creating namespace.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+namespace dev  
+{
+int a=5;
+string personality ="GOD";
+}
+
+using namespace std;
+using namespace dev;
+int main() {
+
+        cout<<a<<" "<<personality;
+
+    return 0;
+}
